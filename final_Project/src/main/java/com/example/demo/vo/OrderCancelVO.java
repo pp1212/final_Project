8b72293191d8b84f8df7079128106f697c4b24ff
envayu <envayu00@gmail.com>
2022-02-24 11:51:22 +0900

    checkout

--- final_Project/src/main/java/com/example/demo/vo/OrderCancelVO.java
+++ final_Project/src/main/java/com/example/demo/vo/OrderCancelVO.java
@@ -1,5 +1,7 @@
 package com.example.demo.vo;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OrderCancelVO {
 
-	private Data order_date;
+	private Date order_date;
 	private int order_no;	
 	private String cust_name;	
 	private String product_img;	
